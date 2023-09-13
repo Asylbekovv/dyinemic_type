53ab0a25b191f12fdd81189055cf81f051560c9e
Asylbekovv <144426976+Asylbekovv@users.noreply.github.com>
2023-09-13 23:45:13 +0600

    Add files via upload

---
+++ dyinemic_type.py
@@ -0,0 +1,14 @@
+def print_name(name):
+    print(name)
+print_name("Arstan")
+print_name = 20
+print(print_name())
+
+
+
+def write_name(last_name):
+    print(last_name)
+write_name("Asylbekov")
+wrt = write_name
+print(wrt)
+
